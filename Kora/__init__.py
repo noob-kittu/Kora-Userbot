50b63c69279d975011e974cab0603965e342cd00
Kittu <70826024+noob-kittu@users.noreply.github.com>
2021-11-04 08:53:18 +0530

    ver 1

--- Kora/__init__.py
+++ Kora/__init__.py
@@ -15,14 +15,14 @@ LOGGER = getLogger(__name__)
 CMD_HELP = {}
 
 # Telegram App KEY and HASH
-API_KEY = os.environ.get("API_KEY", "")
-API_HASH = os.environ.get("API_HASH", "")
+API_KEY = 2191715
+API_HASH = "f8f5367907ae63115bbdce3524b87671"
 
 # Userbot Session String
 STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 # Logging channel/group ID configuration.
-BOTLOG = int(os.environ.get("BOTLOG", ""))
+BOTLOG = int(os.environ.get("BOTLOG_CHATID", ""))
 
 # Bleep Blop, this is a bot ;)
 PM_AUTO_BAN = os.environ.get("PM_AUTO_BAN", "False")
